ae6f4b15380064b49e19861b14b58a84954f5ecf
DimON13124 <dimabukhanoff2705@mail.ru>
2024-06-23 21:43:07 +0300

    сайт

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -58,3 +58,39 @@ with allure.step('Проверка на кликабельность'):
 def test_main_footer(web_browser):
     "Этот тест проверяет футер страницы"
 
+    locators = MainPage(web_browser)
+
+    footer_elements = [
+        (locators.btn_services_to_passengers, "Услуги пассажирам"),
+        (locators.btn_freight_transportation, "Грузовые перевозки"),
+        (locators.btn_tourism_and_rest, "Туризм и отдых"),
+        (locators.btn_corporate, "Корпаротивный"),
+    ]
+
+def test_footer(web_browser, footer_elements):
+
+        for elements, elements_text in footer_elements:
+            check.is_true(elements.is_visible())
+            check.is_true(elements.is_clickable())
+            check.equal(elements.get_text(), elements_text)
+
+@allure.story('Тест для проверки главной страницы')
+@allure.feature('Тест для проверки адресса ссылок')
+def test_footers_link(web_browser):
+    "Этот тест проверяет адреса ссылок футера главной страницы"
+
+locators = MainPage(web_browser)
+
+link_footer_elements = [
+    (locators.btn_services_to_passengers_link, 'https://pass.rw.by/ru/'),
+    (locators.btn_freight_transportation_link, 'https://www.rw.by/cargo_transportation/'),
+    (locators.btn_tourism_and_rest, 'https://www.rw.by/tourism_and_recreation/'),
+    (locators.btn_corporate, 'https://www.rw.by/corporate/'),
+    ]
+
+with allure.step('Проверка на кликабельность'):
+    "Этот тест проверяет ссылки на кликабельность"
+    for elements, links in link_header_elements:
+        check.is_true(elements.is_visible())
+        check.is_true(elements.is_clickable())
+        check.equal(elements.get_attribute('href'), links)
